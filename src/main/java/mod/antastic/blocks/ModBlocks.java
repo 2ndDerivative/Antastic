28e5f43600ee97015be2b45dd11146031404b13e
2ndDerivative <niclas@klugmann.de>
2025-05-09 15:58:31 +0200

    update to 1.21.5

--- src/main/java/mod/antastic/blocks/ModBlocks.java
+++ src/main/java/mod/antastic/blocks/ModBlocks.java
@@ -1,24 +1,28 @@
 package mod.antastic.blocks;
 
+import java.util.function.Function;
+
 import mod.antastic.Antastic;
-import net.minecraft.block.AbstractBlock.Settings;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
 
 public class ModBlocks {
-    public static final Block ANT_HILL = register(new Block(Settings.create()
-        .sounds(BlockSoundGroup.GRASS)
-        .registryKey(registryKey("ant_hill"))), "ant_hill");
+    public static final Block ANT_HILL = register("ant_hill", Block::new, Block.Settings
+        .create()
+        .sounds(BlockSoundGroup.GRASS));
     
-    static RegistryKey<Block> registryKey(String id) {
-        return RegistryKey.of(RegistryKeys.BLOCK, Antastic.identifier(id));
-    }
-    static Block register(Block block, String id) {
-        return Registry.register(Registries.BLOCK, registryKey(id), block);
+    static Block register(String id, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings) {
+        final Identifier identifier = Antastic.identifier(id);
+        final RegistryKey<Block> registryKey = RegistryKey.of(RegistryKeys.BLOCK, identifier);
+        final Block block = Blocks.register(registryKey, factory, settings);
+        Items.register(block);
+        return block;
     }
     public static void initialize() {}
 }
--- src/main/java/mod/antastic/items/ModItems.java
+++ src/main/java/mod/antastic/items/ModItems.java
@@ -1,35 +1,15 @@
 package mod.antastic.items;
 
-import mod.antastic.Antastic;
 import mod.antastic.blocks.ModBlocks;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.util.Identifier;
+import net.minecraft.item.Items;
 
 
 public class ModItems {
-    public static final Item ANT_HILL = register(new BlockItem(ModBlocks.ANT_HILL, new Item.Settings().registryKey(registryKey("ant_hill"))), "ant_hill");
-
-    public static RegistryKey<Item> registryKey(String id) {
-        Identifier itemID = Antastic.identifier(id);
-        RegistryKey<Item> registry_key = RegistryKey.of(RegistryKeys.ITEM, itemID);
-        return registry_key;
-    }
-
-    public static Item register(Item item, String id) {
-        Item registeredItem = Registry.register(Registries.ITEM, registryKey(id), item);
-        return registeredItem;
-    }
-
     public static void initialize() {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register((itemGroup) -> {
-            itemGroup.add(ModBlocks.ANT_HILL.asItem());
+            itemGroup.addAfter(Items.BEE_NEST.asItem(), ModBlocks.ANT_HILL.asItem());
         });
     }
 }
